32b43a2ad9316c95ebd5200a4ee8e08615f686d0
charan-creator <85110032+charan-creator@users.noreply.github.com>
2024-05-19 18:20:57 +0000

    complete CI/CD pipeline and Dockerization

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # MyApp
+
+## setup
+
+```
+git clone  https://github.com/charan-creator/MyApp.git
+
+cd MyApp
+
+```
+Build and Run the application using Docker compose
+
+`docker-compose up`
+
+components :
+
+Go Application : `http://localhost:8080`
+
+Nextjs Application: `http://localhost:3000`
+
+wordpress Application: `http://localhost`
---
+++ wordpress-app/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '10c54de0e1f5752dad481e4c19be45c541916950',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '10c54de0e1f5752dad481e4c19be45c541916950',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.9.2',
+            'version' => '3.9.2.0',
+            'reference' => 'aac1f6f347a5c5ac6bc98ad395007df00990f480',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
